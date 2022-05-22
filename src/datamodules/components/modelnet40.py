d7ec44ed56bbb043ffe0eea295b08cdb1fc6fe39
FuLian <fulian18888920480@gmail.com>
2022-05-23 00:19:18 +0900

    Uplode code

---
+++ src/datamodules/components/modelnet40.py
@@ -0,0 +1,71 @@
+import os
+import glob
+import h5py
+import numpy as np
+from torch.utils.data import Dataset
+
+def download(data_dir):
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+    if not os.path.exists(os.path.join(data_dir, 'modelnet40_ply_hdf5_2048')):
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget %s --no-check-certificate; unzip %s' % (www, zipfile))
+        os.system('mv %s %s' % (zipfile[:-4], data_dir))
+        os.system('rm %s' % (zipfile))
+
+def load_data(data_dir, partition):
+    all_data = []
+    all_label = []
+    for h5_name in glob.glob(os.path.join(data_dir, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+def random_point_dropout(pc, max_dropout_ratio=0.875):
+    ''' batch_pc: BxNx3 '''
+    # for b in range(batch_pc.shape[0]):
+    dropout_ratio = np.random.random()*max_dropout_ratio # 0~0.875    
+    drop_idx = np.where(np.random.random((pc.shape[0]))<=dropout_ratio)[0]
+    # print ('use random drop', len(drop_idx))
+
+    if len(drop_idx)>0:
+        pc[drop_idx,:] = pc[0,:] # set to the first point
+    return pc
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+       
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.02):
+    N, C = pointcloud.shape
+    pointcloud += np.clip(sigma * np.random.randn(N, C), -1*clip, clip)
+    return pointcloud
+
+
+class ModelNet40(Dataset):
+    def __init__(self, data_dir, num_points, partition='train'):
+        self.data, self.label = load_data(data_dir, partition)
+        self.num_points = num_points
+        self.partition = partition        
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        label = self.label[item][0]
+        if self.partition == 'train':
+            pointcloud = random_point_dropout(pointcloud) # open for dgcnn not for our idea  for all
+            pointcloud = translate_pointcloud(pointcloud)
+            np.random.shuffle(pointcloud)
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
